5ccd1cf32a1f800c69e9d6d434c441e621d0f51b
Vadim Brodsky <jamiejakov@me.com>
2025-12-26 17:56:12 +1100

    vercel and tailwind integration into astro done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+// Also can be @astrojs/vercel/static
+import vercel from '@astrojs/vercel/serverless';
+import tailwindcss from "@tailwindcss/vite";
 
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
   site: 'https://blog.brodsky.au',
   integrations: [preact()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
