d631df8f7524e0dfddbce3a135bbb878cbdabec7
Vadim Brodsky <jamiejakov@me.com>
2025-12-27 13:59:22 +1100

    alright we've learned it all

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-// Also can be @astrojs/vercel/static
-import vercel from '@astrojs/vercel/serverless';
-import tailwindcss from "@tailwindcss/vite";
+import vercel from '@astrojs/vercel';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
