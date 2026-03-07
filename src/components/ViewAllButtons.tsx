d2375535c5a133e6a21926371ad05e39cf3a92ab
Vadim Brodsky <jamiejakov@me.com>
2026-03-07 10:24:52 +0900

    minor fixes

--- src/components/ViewAllButtons.tsx
+++ src/components/ViewAllButtons.tsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon, Shapes, Tag } from 'lucide-react';
 
 export const ViewAllTagsButton: React.FC = () => {
   return (
-    <Button asChild={true} variant="outline" className="text-l text-primary h-auto py-1 self-center">
+    <Button asChild={true} variant="outline" className="text-l text-primary h-auto py-1">
       <a href="/tags/">
         <Tag className="size-4" />
         All tags <ArrowRightIcon className="size-6" />
@@ -14,7 +14,7 @@ export const ViewAllTagsButton: React.FC = () => {
 
 export const ViewAllCategoriesButton: React.FC = () => {
   return (
-    <Button asChild={true} variant="outline" className="text-l text-primary h-auto py-1 self-center">
+    <Button asChild={true} variant="outline" className="text-l text-primary h-auto py-1">
       <a href="/categories/">
         <Shapes className="size-4" />
         All categories <ArrowRightIcon className="size-6" />
