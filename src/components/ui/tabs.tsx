9563b863d3e681897825248bb10d5824a140a3d9
Vadim Brodsky <jamiejakov@me.com>
2026-08-16 23:04:46 +1000

    fix header z-index

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -94,13 +94,13 @@ function TabsTrigger({ className, children, value, ...props }: React.ComponentPr
       data-slot="tabs-trigger"
       value={value}
       className={cn(
-        `relative inline-flex h-[calc(100%-1px)] flex-1 cursor-pointer items-center justify-center gap-1.5 rounded-md
-        border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap text-foreground/60 transition-[color]
-        group-data-[orientation=vertical]/tabs:w-full group-data-[orientation=vertical]/tabs:justify-start
-        hover:text-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50
-        focus-visible:outline-1 focus-visible:outline-ring disabled:pointer-events-none disabled:opacity-50
-        data-[state=active]:text-foreground dark:text-muted-foreground dark:hover:text-foreground
-        dark:data-[state=active]:text-foreground [&_svg]:pointer-events-none [&_svg]:shrink-0
+        `relative isolate inline-flex h-[calc(100%-1px)] flex-1 cursor-pointer items-center justify-center gap-1.5
+        rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap text-foreground/60
+        transition-[color] group-data-[orientation=vertical]/tabs:w-full
+        group-data-[orientation=vertical]/tabs:justify-start hover:text-foreground focus-visible:border-ring
+        focus-visible:ring-[3px] focus-visible:ring-ring/50 focus-visible:outline-1 focus-visible:outline-ring
+        disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-foreground dark:text-muted-foreground
+        dark:hover:text-foreground dark:data-[state=active]:text-foreground [&_svg]:pointer-events-none [&_svg]:shrink-0
         [&_svg:not([class*='size-'])]:size-4`,
         className
       )}
